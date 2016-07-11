754db6b3e72c233935c25b5833194c7e59e06e80
Tim Ehlers <Tim.Ehlers@orbitz.com>
2016-07-11 13:27:46 -0500

    added file

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'gemserver'
+maintainer       'Steve Hoffman'
+maintainer_email 'steve.hoffman@orbitz.com'
+license          'All rights reserved'
+description      'Installs/Configures gemserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+supports         'centos'
+depends          'nfs'
