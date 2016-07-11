a82204bb0fd35130df9e7e02b0eddeadf992367f
Tim Ehlers <Tim.Ehlers@orbitz.com>
2016-07-11 17:14:41 -0500

    fixed the ordering and regex

--- verifymetadata.groovy
+++ verifymetadata.groovy
@@ -2,7 +2,8 @@ System.out.println("Check for proper metadata.rb/metadata.json tags");
 
 import groovy.io.FileType
 import java.util.regex.*
-Pattern pattern    = Pattern.compile("(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.])");
+//Pattern version_pattern    = Pattern.compile("(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.])");
+//Pattern metadataver_pattern   = Pattern.compile("(^(version)\\s+.(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.]).)");
 
 def list = []
 
@@ -11,19 +12,27 @@ dir.eachFileRecurse (FileType.FILES) { file ->
   list << file
 }
 
-list.each {
-  Matcher matcher    = pattern.matcher(it.path);
-  boolean matchFound = matcher.find();
-  if (matchFound) {
-    String version = matcher.group(1);
-    new File( 'testing-jenkins/metadata.rb' ).text.tokenize( '\n' ).findAll {
-      it.matches "^(version\\s+.${version}.)"
-    }.each {
-    System.out.println("found version number: " + version);
-    }
-  }
-  else {
-    System.out.println("No valid versions found, please commit metadata.rb and tags that match");
-    System.exit(1)
-  }
+def metadata_regex = ~/(^(version)\s+.(?!\.)(\d+(\.\d+)+)(?![\d\.]).)/
+def versions = []
+new File('testing-jenkins/metadata.rb').eachLine { line ->
+        def matcher_meta = metadata_regex.matcher(line)
+        while (matcher_meta.find()) {
+          versions << matcher_meta.group(3)
+        }
 }
+
+System.out.println("Found the following versions in metadata: " + versions );
+
+def version_regex = ~/${versions[0]}/
+list.each {
+  def matcher_version = version_regex.matcher(it.name)
+  boolean matchFound = matcher_version.find();
+  println it.name
+  if (matchFound) {
+    System.out.println("found version number: " + it.name);
+    System.exit(0);
+    }
+}
+
+System.out.println("Cannot find valid version");
+System.exit(1);
