77afd895e13a401b5087f7aee7c2fd13f842990d
Tim Ehlers <Tim.Ehlers@orbitz.com>
2016-07-12 13:33:09 -0500

    cannot use the file system as these arent always here, have to shell out

--- verifymetadata.groovy
+++ verifymetadata.groovy
@@ -2,19 +2,22 @@ System.out.println("Check for proper metadata.rb/metadata.json tags");
 
 import groovy.io.FileType
 import java.util.regex.*
+import hudson.model.*
+
 //Pattern version_pattern    = Pattern.compile("(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.])");
 //Pattern metadataver_pattern   = Pattern.compile("(^(version)\\s+.(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.]).)");
 
-def list = []
-
-def dir = new File("testing-jenkins/.git/refs/tags")
-dir.eachFileRecurse (FileType.FILES) { file ->
-  list << file
+//def list = []
+def tagList(dir) {
+        def command = [ "/bin/bash", "-c", "cd '${dir}' ; git fetch --tags &> /dev/null ; git tag -l" ]
+        def process = command.execute(); process.waitFor()
+        def list = process.in.text.tokenize("\n")
 }
+
 
 def metadata_regex = ~/(^(version)\s+.(?!\.)(\d+(\.\d+)+)(?![\d\.]).)/
 def versions = []
-new File('testing-jenkins/metadata.rb').eachLine { line ->
+new File( jenkinsJob().workspace'/metadata.rb').eachLine { line ->
         def matcher_meta = metadata_regex.matcher(line)
         while (matcher_meta.find()) {
           versions << matcher_meta.group(3)
@@ -23,16 +26,26 @@ new File('testing-jenkins/metadata.rb').eachLine { line ->
 
 System.out.println("Found the following versions in metadata: " + versions );
 
-def version_regex = ~/${versions[0]}/
-list.each {
-  def matcher_version = version_regex.matcher(it.name)
-  boolean matchFound = matcher_version.find();
-  println it.name
-  if (matchFound) {
-    System.out.println("found version number: " + it.name);
-    System.exit(0);
-    }
+try {
+  println "Trying to find version";
+  def tagList = tagList( jenkinsJob().workspace )
+  def version_regex = ~/${versions[0]}/
+  tagList.each {
+    def matcher_version = version_regex.matcher(it)
+    boolean matchFound = matcher_version.find();
+    //println it.name
+    if (matchFound) {
+      System.out.println("found version number: " + it);
+      System.exit(0);
+      }
+  }
+
+} catch ( e ) {
+  [ e.toString() ]
+
 }
 
-System.out.println("Cannot find valid version");
+
+
+System.out.println("Cannot find valid version in your commit, please update metadata.rb and tag the repo");
 System.exit(1);
